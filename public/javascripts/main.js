e457b4169b431a7b41d8942cbebb7a341f0bb638
Le Minh <le.minh011100@gmail.com>
2021-04-14 21:30:34 +0700

    Update 14/04

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,14 +20,15 @@ window.addEventListener('scroll', function(){
 })
 
 // Edit content
-
+var templates = [];
+document.querySelectorAll('.list-group:not(#group1, .education) .hover:first-child').forEach(el => {
+    templates.push(el.innerHTML);
+})
 
 var item = Array.from(document.querySelectorAll('.list-group-item'));
 $('.fa-trash').click(function(){
     $(this).siblings().remove();
 });
-
-
 item.forEach((el, ind) => {
     el.setAttribute('contentEditable', 'true');
     var ck = CKEDITOR.inline(item[ind], {
@@ -35,12 +36,51 @@ item.forEach((el, ind) => {
     })
 })
 
+$('.wrap-chosen').hover(function () {
+        $(this).find('.fa-plus').css('opacity', '1');
+    }, function () {
+        // out
+        $(this).find('.fa-plus').css('opacity', '0');
+    }
+);
+
 $('.fa-plus').click(function(){
-    var text = $(this).parent().html();
-    $(this).parent().after(
-        `<div class="hover address">
-            ${text}
-        </div>`);
+    $(this).parent().siblings('#group2').append(
+        `<div class="hover">
+            <i class="fa fa-arrows" aria-hidden="true"></i>
+            <i class="fa fa-trash" aria-hidden="true"></i>
+            <div class="list-group-item">
+            <span>EMPTY</span>
+            </div>
+        </div>`
+    );
+    $(this).parent().siblings('#group3').append(
+        `<div class="hover">
+            <i class="fa fa-arrows" aria-hidden="true"></i>
+            <i class="fa fa-trash" aria-hidden="true"></i>
+            <div class="hover d-flex justify-content-between">
+            <div class="list-group-item academic col-9">
+                <h4><a class="text-primary" href="#">VARNA TECHNICAL UNIVERSITY</a>, BULGARIA</h4>
+                <div class="mb-3">PHD DEGREE IN INFORMATION AND COMPUTER SCIENCES</div>
+                <span class="text-secondary">PhD thesis: Architectural model of a class numerical computing machine and its application on generating smooth curves and surface</span>
+            </div>
+            <div class="academic col-3">
+                <div class="list-group-item time">
+                3/1991 - 12/1995
+                </div>
+            </div>
+            </div>
+        </div>`
+    );
+    $(this).parent().siblings(`#group3, #group4, #group5, #group6,
+    #group7, #group8, #group9, #group10, #group11, #group12, #group13, #group14`).append(
+        `<div class="hover">
+            <i class="fa fa-arrows" aria-hidden="true"></i>
+            <i class="fa fa-trash" aria-hidden="true"></i>
+            <div class="list-group-item text-secondary">Empty</div>
+        </div>`
+    );
+    $('.toast').toast('show');
     var item = Array.from(document.querySelectorAll('.list-group-item'));
     item.forEach((el, ind) => {
         el.setAttribute('contentEditable', 'true');
