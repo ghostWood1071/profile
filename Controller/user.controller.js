98c225e7412e1fb666b522a75576f1b5ac4b1c2a
ghostWood1071 <thinhquangshin@gmail.com>
2021-04-18 14:29:19 +0700

    add login cookie, login security

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -27,11 +27,64 @@ module.exports.writeData =  function(req, res, next){
     });
 }
 
+
+module.exports.auth = function(req, res, next){
+    if(req.signedCookies.userID){
+        next();
+        return;
+    } else
+        res.redirect("/users/login");
+}
+
+module.exports.getLoginPage  = function(req, res, next){
+    if(req.signedCookies.userID){
+        res.redirect("/users");
+        return;
+    }
+    res.render('login');
+}
+
+module.exports.getLoginInfo = function(req,res,next){
+    var account = req.body.account;
+    var pass = req.body.password;
+    res.locals.login = {
+        account: account, 
+        pass: pass
+    };
+    console.log(res.locals.login);
+    next();
+}
+
+module.exports.validate = function(req,res,next) {
+    var loginInfo = res.locals.login;
+    var data =  JSON.parse(res.locals.stringData);
+    var uid = data.findIndex(x=>x.login.account === loginInfo.account);
+
+    if(uid>=0){
+        var user = data[uid];
+        if(user.login.pass === loginInfo.pass){
+           res.locals.uid = uid;
+           next();
+           return;
+        }
+    }
+    res.send("fail");
+}
+
+module.exports.saveLoginCookie = function(req, res, next){
+    var uid = res.locals.uid;
+    res.cookie("userID",uid,{
+        signed: true
+    });
+    res.redirect("/users");
+}
+
 module.exports.getUserInfo = function (req, res, next) {
     var data = JSON.parse(res.locals.stringData);
-    var uid = res.locals.uid;
+    var uid = req.signedCookies.userID;
     var user = data[uid];
-    res.render('template1', {
+    
+    res.render(user.template, {
         avatar: user.avatar,
         about: user.about,
         research_interest: user.research_interests,
