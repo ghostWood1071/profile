85073532b43ce9c2bb545de24834ff032321733a
Ba Son <bagiangson2001@gmail.com>
2021-04-21 09:31:17 +0700

    Update getdata

--- public/javascripts/getData.js
+++ public/javascripts/getData.js
@@ -2,6 +2,11 @@
 function ignoreDoubleSpaces(string) {
     return string.replace(/ {2,}/g, ' ').trim();
 }
+//replace all
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 //id:id , indexOfChild1: First child work with, indexOfDatas: Index of child Of each child of indexOfChild1 to get data and link
 function getChildsDataAndLinkByID1(id,indexOfChild1,indexOfDatas){
     var result=[];
@@ -259,7 +264,9 @@ document.getElementById(id+'_content').innerHTML = string;
     for(var i =0 ; i <count ; i++){
         var pub_content="";
         for(var k = 0 ; k < publication[1][i].length;k++){
-            pub_content+=`"`+publication[1][i][k]+`",`
+            console.log(pub_content);
+            pub_content+=`"`+publication[1][i][k].toString().replaceAll(`"`,`##`)+`",`
+            console.log(pub_content);
         }
         pub_content=pub_content.substring(0,pub_content.length-1)
         pub +=`{
@@ -269,18 +276,17 @@ document.getElementById(id+'_content').innerHTML = string;
     }
     pub=pub.substring(0,pub.length-1);
 
-    console.log(pub);
-    json_String=`
+    json_String=`[{
     "about":{
         "name": "`+name+`",
         "level":"`+position+`",
         "university": [
             `+university+`
-        ]
+        ],
         "address":"`+address+`",
         "phone": "`+phone+`",
         "fax": "`+fax+`",
-        "mail": "`+mail+`",
+        "mail": `+mail+`
     },
     "research_interests": `+research_int+`,
     "academic": `+academic_s+`,
@@ -295,12 +301,186 @@ document.getElementById(id+'_content').innerHTML = string;
     "publications":[
         `+pub+`
     ]
+}]
     `
-    console.log(json_String);
+
+    console.log(JSON.parse(json_String));
 
 //Save
 function Save(){
-    
+    var name = getDataByID('name');
+    var position = getDataByID('position')
+    var work = getChildsDataAndLinkByID1('work',1,1)
+    var contact = getChildsDataByID('contact',1,0);
+    contact[3]=contact[3].trim();
+    contact[3]=contact[3].split('\n');
+    for(var i = 0 ; i<contact[3].length;i++){
+        contact[3][i]=contact[3][i].trim();
+    }
+    var research_interests =getChildsDataByID('sortlist1',1,0)
+    //
+    var academicItemName = getDataByClassName('academic-item-name');
+    var academicItemLocation = getDataByClassName('academic-item-location');
+    var academicLevel = getDataByClassName('academic-level');
+    var academicDescription= getDataByClassName('academic-description');
+    var academicItemTime= getDataByClassName('academic-item-time');
+    var academic =[]
+    if(academicItemName.length!=0){
+        var count=academicItemName.length;
+    }
+    for(var i = 0 ; i <count;i++){
+        academic.push([academicItemName[i],academicItemLocation[i],academicLevel[i],academicDescription[i],academicItemTime[i]])
+    }
+    //
+    var teachingYear=getDataByClassName('teaching-year');
+    var teaching_content=getDataByClassNameHaveChilds('teaching-content')
+    var teaching=[]
+    for( var i = 0 ; i<teachingYear.length;i++){
+        teaching.push([teachingYear[i],teaching_content[i]])
+    }
+    //
+    thesis_link=getChildsDataAndLinkByID2('sortlist3',1);
+    thesis_name=getDataByClassName('thesis-year')
+    thesis_content=getDataByClassNameHaveChilds('thesis-content')
+    thesis=[]
+    thesis.push(thesis_link,[thesis_name,thesis_content])
+    //
+    research=getDataByClassNameHaveChilds('grant-list')
+    //
+    publication=[]
+    publication_year=getDataByClassName('publication-year')
+    publication_content=getDataByClassNameHaveChilds('publication-content')
+    publication.push(publication_year,publication_content)
+    //
+    //Convert To Json String
+     var university ="";
+     for(var i = 0 ; i<work.length;i++){
+        university+=`{
+            "name": "`+work[i][0]+`",
+            "link": "`+work[i][1]+`"
+        },`
+     }
+     university=university.substring(0,university.length-1);
+
+     address=contact[0];
+     phone=contact[1];
+     fax=contact[2];
+     mail=`["`+contact[3][0]+`","`+contact[3][1]+`"]`
+     
+     research_int="[";
+     for(var i = 0 ; i<research_interests.length;i++){
+         research_int+=`"`+research_interests[i]+`",`
+     }
+     research_int=research_int.substring(0,research_int.length-1);
+     research_int+="]";
+     
+
+     academic_s=``;
+     for(var i = 0;i<academic.length;i++){
+        academic_s+=`{
+            "university": "`+academic[i][0]+`",
+          "location": "`+academic[i][1]+`",
+          "level": "`+academic[i][2]+`",
+          "descript": "`+academic[i][3]+`",
+          "time": "`+academic[i][4]+`"
+        },`
+     }
+     academic_s=academic_s.substring(0,academic_s.length-1);
+
+     teaching_s="";
+     for(var i =0 ; i<teaching.length;i++){
+         var course = "[";
+         for(var k = 0 ; k <teaching[i][1].length;k++){
+             course+=`"`+teaching[i][1][k]+`",`
+         }
+         course=course.substring(0,course.length-1);
+         course+=`]`;
+         teaching_s+=`{
+            "year": "`+teaching[i][0]+`",
+            "course": `+course+`
+         },`
+     }
+     teaching_s=teaching_s.substring(0,teaching_s.length-1);
+     
+     var count =thesis[0].length
+     var link="[";
+     for(var i = 0 ; i <count;i++){
+         link+=`{
+             "name" : "`+thesis[0][i][0]+`",
+             "address" : "`+thesis[0][i][1]+`"
+         },`
+     }
+     link=link.substring(0,link.length-1);
+     link+=`]`
+
+     var content =`[`
+    var count =thesis[1][0].length;
+    for(var i = 0 ; i < count ; i ++){
+        var list_content="";
+        for(var k =0; k < thesis[1][1][i].length;k++){
+            list_content+=`"`+thesis[1][1][i][k]+`",`
+        }
+        list_content=list_content.substring(0,list_content.length-1);
+        content+=`{
+            "name": "`+thesis[1][0][i]+`",
+            "list-content": [`+list_content+`]
+        },`
+    }
+    content=content.substring(0,content.length-1);
+    content+=`]`
+
+    research_content="";
+    for(var i = 0 ;i<research[0].length;i++){
+        research_content+=`"`+research[0][i]+`",`
+    }
+    research_content=research_content.substring(0,research_content.length-1);   
+
+    var count = publication[0].length;
+    var pub = "";
+    for(var i =0 ; i <count ; i++){
+        var pub_content="";
+        for(var k = 0 ; k < publication[1][i].length;k++){
+            console.log(pub_content);
+            pub_content+=`"`+publication[1][i][k].toString().replaceAll(`"`,`##`)+`",`
+            console.log(pub_content);
+        }
+        pub_content=pub_content.substring(0,pub_content.length-1)
+        pub +=`{
+        "year": "`+publication[0][i]+`",
+        "content": [`+pub_content+`]
+        },`
+    }
+    pub=pub.substring(0,pub.length-1);
+
+    json_String=`[{
+    "about":{
+        "name": "`+name+`",
+        "level":"`+position+`",
+        "university": [
+            `+university+`
+        ],
+        "address":"`+address+`",
+        "phone": "`+phone+`",
+        "fax": "`+fax+`",
+        "mail": `+mail+`
+    },
+    "research_interests": `+research_int+`,
+    "academic": `+academic_s+`,
+    "teaching": `+teaching_s+`,
+    "thesis": {
+        "link": `+link+`,
+        "content": `+content+`
+    },
+    "research_grant":{
+        "content" :[`+research_content+`]
+    },
+    "publications":[
+        `+pub+`
+    ]
+}]
+    `
+
+    return JSON.parse(json_String);
 }
 
 
