bc807119a867a9b7e6c3c62dc5392960f0b8f6ce
ghostWood1071 <thinhquangshin@gmail.com>
2021-05-27 17:00:43 +0700

    lol

--- public/javascripts/checkPass.js
+++ public/javascripts/checkPass.js
@@ -2,7 +2,11 @@ function checkPassword(){
     var password =  $("#oldPass").val();
     $.post("/users/passverify", {currentPass:password},
         function (data, textStatus, jqXHR) {
-            console.log(data);
+            if(data.head){
+                $($(".form-error")[1]).css("opacity","0");
+            }else{
+                $($(".form-error")[1]).css("opacity","1");
+            }
         },
     );
 }
